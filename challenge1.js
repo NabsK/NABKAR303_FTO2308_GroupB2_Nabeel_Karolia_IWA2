3910318791c75736be7d4274fbc93c3037b757c4
Nabeel Karolia <Nab.kar123@gmail.com>
2023-10-18 10:34:52 +0200

    fixed all errors

--- challenge1.js
+++ challenge1.js
@@ -1,9 +1,8 @@
-/* This is the firstname of the user /*
+/* This is the first name of the user */
 
-const user = 'John'
+const user = "John";
 
+/* This is the last name of the user */
+const surname = "Smith";
 
-/* This is the lastname of the user /* const surname = 'Smith'
-
-
-console.log(user; surname;)
+console.log(user, surname);
